85c5a183e7e9ed23ad88b4e09ae16d8c505365b5
duyhuynh24 <huynhndpc06610@fpt.edu.vn>
2025-03-30 11:44:45 +0700

    Tạo giao diện
    Cắt giao diện giỏ hàng, đăng nhập, đăng kí và route chuyển trang

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import ClientLayout from "./layouts/Client/ClientLayout";
 import Home from "./pages/Client/Home/Home";
 import "./assets/styles/global.css"
 import Shop from "./pages/Client/Shop/Shop";
+import Login from "./pages/Client/Login/login";
+import Register from "./pages/Client/Register/register";
+import Cart from "./pages/Client/Cart/cart";
+
+
+
 
 function App() {
   return (
@@ -11,7 +17,13 @@ function App() {
         <Route path="/" element={<ClientLayout />}>
           <Route index element={<Home />} />
           <Route path="shop" element={<Shop />} />
+          <Route path="cart" element={<Cart />} />
         </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+       
+      
+       
       </Routes>
     </BrowserRouter>
   );
